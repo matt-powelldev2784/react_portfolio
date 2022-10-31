c1145655c8e472dc0e4809c2ec0ab630e9290716
Matthew Powell <matt.powell2784@gmail.com>
2022-10-31 21:22:35 +0000

    carousel naviagtion update

--- src/components/03_Projects/Card/CardCarousel.jsx
+++ src/components/03_Projects/Card/CardCarousel.jsx
@@ -31,15 +31,19 @@ export const CardCarousel = () => {
     const currentFlipCard = <FlipCard cardProps={flipCardInfo[flipCardNumber]} />
 
     return (
-        <StyledFlexBox>
-            <StyledPrevArrowP onClick={onPrevFlipCard}>{'⏴'}</StyledPrevArrowP>
+        <StyledContainerDiv>
+            <StyledPrevCircleDiv>
+                <StyledPrevArrowP onClick={onPrevFlipCard}>{'⬅'}</StyledPrevArrowP>
+            </StyledPrevCircleDiv>
             {currentFlipCard}
-            <StyledNextArrowP onClick={onNextFlipCard}>{'⏵'}</StyledNextArrowP>
-        </StyledFlexBox>
+            <StyledNextCircleDiv>
+                <StyledNextArrowP onClick={onNextFlipCard}>{'⮕'}</StyledNextArrowP>
+            </StyledNextCircleDiv>
+        </StyledContainerDiv>
     )
 }
 
-const StyledFlexBox = styled.div`
+const StyledContainerDiv = styled.div`
     position: absolute;
     display: block;
     margin: 0;
@@ -51,42 +55,82 @@ const StyledFlexBox = styled.div`
     }
 `
 
-const StyledNextArrowP = styled.p`
+const StyledPrevCircleDiv = styled.div`
     position: absolute;
     display: block;
-    margin: 0;
-    right: 0.5rem;
     top: 50%;
     transform: translateY(-50%);
+    width: 6rem;
+    height: 6rem;
+    left: -8rem;
+    border-radius: 50%;
+    border: 2px solid #ffffff;
 
-    color: #ffffff;
-    font-size: 10rem;
-    font-family: 'Noto Sans Symbols 2', sans-serif;
-    margin: auto;
+    background: #000000;
     z-index: 1;
 
     @media (max-device-width: 440px) {
-        font-size: 5rem;
-        right: -1rem;
+        width: 3rem;
+        height: 3rem;
+        left: -1.4rem;
     }
 `
 
 const StyledPrevArrowP = styled.p`
     position: absolute;
-    display: block;
-    margin: 0;
-    left: 0.5rem;
-    top: 50%;
-    transform: translateY(-50%);
+    top: 3.5rem;
+    left: 2.75rem;
+    transform: translate(-50%, -50%);
 
     color: #ffffff;
-    font-size: 10rem;
+    font-size: 4.5rem;
     font-family: 'Noto Sans Symbols 2', sans-serif;
     margin: auto;
     z-index: 1;
 
     @media (max-device-width: 440px) {
-        font-size: 5rem;
-        left: -1rem;
+        font-size: 2rem;
+        top: 1.6rem;
+        left: 1.3rem;
+    }
+`
+
+const StyledNextCircleDiv = styled.div`
+    position: absolute;
+    display: block;
+    top: 50%;
+    transform: translateY(-50%);
+    width: 6rem;
+    height: 6rem;
+    right: -8rem;
+    border-radius: 50%;
+    border: 2px solid #ffffff;
+
+    background: #000000;
+    z-index: 1;
+
+    @media (max-device-width: 440px) {
+        width: 3rem;
+        height: 3rem;
+        right: -1.4rem;
+    }
+`
+
+const StyledNextArrowP = styled.div`
+    position: absolute;
+    top: 3.5rem;
+    left: 3rem;
+    transform: translate(-50%, -50%);
+
+    color: #ffffff;
+    font-size: 4.5rem;
+    font-family: 'Noto Sans Symbols 2', sans-serif;
+    margin: auto;
+    z-index: 1;
+
+    @media (max-device-width: 440px) {
+        font-size: 2rem;
+        top: 1.6rem;
+        left: 1.45rem;
     }
 `

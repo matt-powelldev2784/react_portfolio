ff29890c82f33b4a50b4b06f503402c9d321c395
Matthew Powell <matt.powell2784@gmail.com>
2022-11-06 17:28:06 +0000

    about text update

--- src/components/02_About/aboutTextInfo.js
+++ src/components/02_About/aboutTextInfo.js
@@ -1,7 +1,7 @@
 const text1 = "Hi, my name is Matthew Powell, I'm a software developer looking to land my first professional role."
 const text2 = 'I have full stack experience and have developed solutions with JavaScript, React, Redux, TypeScript, NodeJS and GIT.'
 const text3 =
-    'I love to code, I enjoy the challenge of learning new technologies. I get a buzz when solving problems or completing new features in my projects.'
+    'I love to code. I enjoy the challenge of learning new technologies. I get a buzz when solving problems or completing new features in my projects.'
 const text4 =
     'I like to keep fit and I am a keen footballer and swimmer. I have recently become a dad for the first time and enjoy spending time with my daughter.'
 export const text5 =

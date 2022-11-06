0d472352709bc1a8dd7fd1ba35019bdf05213e5e
Matthew Powell <matt.powell2784@gmail.com>
2022-11-06 13:32:26 +0000

    about responsive update

--- src/components/02_About/AboutText.jsx
+++ src/components/02_About/AboutText.jsx
@@ -5,7 +5,7 @@ import TypeWriterEffect from 'react-typewriter-effect'
 
 export const AboutText = () => {
     const { theme } = React.useContext(ThemeContext)
-    const { ABOUT_H1_BG, ABOUT_H1_TEXT, ABOUT_TEXT } = theme?.colors?.about
+    const { ABOUT_TEXT } = theme?.colors?.about
 
     const text =
         "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum"
